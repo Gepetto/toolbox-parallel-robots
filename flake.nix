1ee468809d408d530a0dae1b209bb8a082bd8422
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-05-02 20:12:59 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,38 +1,23 @@
 {
   description = "Set of tools to work with robots with bilateral constraints";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flakoboros.follows = "gepetto/flakoboros";
-    gazebros2nix.follows = "gepetto/gazebros2nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.pyOverrideAttrs.toolbox-parallel-robots = _: _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./package.xml
-                  ./toolbox_parallel_robots
-                ];
-              };
-            };
-          }
-        ];
+        pyOverrideAttrs.toolbox-parallel-robots = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./package.xml
+              ./toolbox_parallel_robots
+            ];
+          };
+        };
       }
     );
 }
